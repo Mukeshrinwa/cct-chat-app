ea862c2b3a429cf3aa516bc3943483cedc7a5faf
Manish Kumar <manishmuhal89@gmail.com>
2026-06-09 15:28:42 +0530

    new

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // ----------------------------------------------------------------------
 
-const PORT = 8080;
+const PORT = 5001;
 
 const _env = loadEnv('all', process.cwd());
 
